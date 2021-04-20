700af99962fa87dbc4cc2fa82d5f727706dff8fb
qitas <qitas@qitas.cn>
2021-04-20 10:24:26 +0800

    fix

---
+++ extend/opencpu.py
@@ -0,0 +1,15 @@
+from SCons.Script import DefaultEnvironment
+
+env = DefaultEnvironment()
+platform = "opencpu"
+module = platform + "-" + env.BoardConfig().get("build.core")
+m = __import__(module)
+globals()[module] = m
+m.dev_init(env, platform)
+#print( env.Dump() )
+
+
+
+
+
+
