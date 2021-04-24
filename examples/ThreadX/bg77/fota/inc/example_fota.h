f1cbeb7bafe0e45e18ef22d0f1df2c9c5cbcf65f
qitas <qitas@qitas.cn>
2021-04-24 19:52:24 +0800

    init

--- examples/ThreadX/bg77/fota/inc/example_fota.h
+++
@@ -1,9 +0,0 @@
-/******************************************************************************
-*@file    example_fota.h
-*@brief   example of fota operation
-*******************************************************************************/
-#ifndef __EXAMPLE_FOTA_H__
-#define __EXAMPLE_FOTA_H__
-
-#endif /*__EXAMPLE_FOTA__*/
-
