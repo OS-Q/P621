48bb23ded42db4e5efdeaee44d9f271f42d10008
qitas <qitas@qitas.cn>
2021-07-10 18:01:09 +0800

    fix

--- builder/frameworks/opencpu-bc66.py
+++ builder/frameworks/opencpu-bc66.py
@@ -22,7 +22,7 @@ def dev_create_template(env):
     D = join(env.subst("$PROJECT_DIR"), "config")
     if False == os.path.isdir(D):
         os.makedirs(D)
-        S = join(env.PioPlatform().get_package_dir("framework-quectel"), "templates", env.BoardConfig().get("build.core"))
+        S = join(env.PioPlatform().get_package_dir("F621"), "templates", env.BoardConfig().get("build.core"))
         F = [
             "custom_feature_def.h",
             "custom_gpio_cfg.h",
@@ -36,7 +36,7 @@ def dev_create_template(env):
             if False == os.path.isfile(dst):
                 copyfile(join(S, I), dst)
     D = join(env.subst("$PROJECT_DIR"), "src")
-    S = join(env.PioPlatform().get_package_dir("framework-quectel"), "templates", env.BoardConfig().get("build.core"))
+    S = join(env.PioPlatform().get_package_dir("F621"), "templates", env.BoardConfig().get("build.core"))
     if False == os.path.isfile( join(D, "main.c") ) and False == os.path.isfile( join(D, "main.cpp") ):
         copyfile( join(S, "main.c"), join(D, "main.c") )
 
@@ -63,7 +63,7 @@ def dev_compiler(env):
 def dev_init(env, platform):
     dev_create_template(env)
     dev_compiler(env)
-    framework_dir = env.PioPlatform().get_package_dir("framework-quectel")
+    framework_dir = env.PioPlatform().get_package_dir("F621")
     core = env.BoardConfig().get("build.core")
     SDK = join(framework_dir, platform, core,  env.BoardConfig().get("build.sdk", "SDK15"))
     disable_nano = env.BoardConfig().get("build.disable_nano", "0") # defaut nano is enabled
