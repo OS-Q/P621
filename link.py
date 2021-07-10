bc59a0b482395db850f570c725c8867207f2f02e
qitas <qitas@qitas.cn>
2021-07-10 18:06:32 +0800

    fix

--- link.py
+++ link.py
@@ -3,9 +3,4 @@ from platformio.managers.platform import PlatformBase
 
 class P621Platform(PlatformBase):
     def configure_default_packages(self, variables, targets):
-        framework = variables.get("pioframework", [])
-        if variables["board"].startswith('ec2'):
-            del self.packages["toolchain-gccarmnoneeabi"]
-        else:
-            del self.packages["toolchain-gcc-ec2x"]
         return PlatformBase.configure_default_packages(self, variables, targets)
